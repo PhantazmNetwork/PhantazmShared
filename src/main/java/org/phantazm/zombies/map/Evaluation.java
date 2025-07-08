55bb8adcd6d19046c4fe5971008c4b3b3da827db
steank <dnd.initiative.tracker@gmail.com>
2025-07-08 02:51:42 -0700

    fix CumulativeTransactionModifierInteractor, StringTagPredicate now has a default value, add NONE_TRUE

--- src/main/java/org/phantazm/zombies/map/Evaluation.java
+++ src/main/java/org/phantazm/zombies/map/Evaluation.java
@@ -7,7 +7,8 @@ import java.util.function.BiPredicate;
 
 public enum Evaluation {
     ALL_TRUE,
-    ANY_TRUE;
+    ANY_TRUE,
+    NONE_TRUE;
 
     public <T, V> boolean evaluate(@NotNull Iterable<? extends BiPredicate<T, V>> predicates, T type, V second) {
         Objects.requireNonNull(predicates);
@@ -15,21 +16,24 @@ public enum Evaluation {
         return switch (this) {
             case ALL_TRUE -> {
                 for (BiPredicate<T, V> predicate : predicates) {
-                    if (!predicate.test(type, second)) {
-                        yield false;
-                    }
+                    if (!predicate.test(type, second)) yield false;
                 }
 
                 yield true;
             }
             case ANY_TRUE -> {
                 for (BiPredicate<T, V> predicate : predicates) {
-                    if (predicate.test(type, second)) {
-                        yield true;
-                    }
+                    if (predicate.test(type, second)) yield true;
                 }
 
                 yield false;
+            }
+            case NONE_TRUE -> {
+                for (BiPredicate<T, V> predicate : predicates) {
+                    if (predicate.test(type, second)) yield false;
+                }
+
+                yield true;
             }
         };
     }
